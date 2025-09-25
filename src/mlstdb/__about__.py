427411414a6a4c5b72a51e6884e79133cf6e4969
Himal Shrestha <stha.himal2007@gmail.com>
2025-09-25 12:32:26 +1000

    ⬆️ Bump version to 0.1.6

--- src/mlstdb/__about__.py
+++ src/mlstdb/__about__.py
@@ -1,4 +1,9 @@
 # SPDX-FileCopyrightText: 2025-present Himal Shrestha <himal2007@duck.com>
 #
+<<<<<<< HEAD
 # SPDX-License-Identifier: GPL-3.0-or-later
 __version__ = "0.1.5"
+=======
+# SPDX-License-Identifier: MIT
+__version__ = "0.1.6"
+>>>>>>> 931f5d6 (⬆️ Bump version to 0.1.6)
