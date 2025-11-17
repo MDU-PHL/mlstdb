ff413271cfa7b9a8fd4149daa0d35e710280df3f
Himal Shrestha <stha.himal2007@gmail.com>
2025-11-18 05:39:18 +1100

    chore (merge): sync with main branch

--- src/mlstdb/__about__.py
+++ src/mlstdb/__about__.py
@@ -1,18 +1,4 @@
 # SPDX-FileCopyrightText: 2025-present Himal Shrestha <himal2007@duck.com>
 #
-<<<<<<< HEAD
-<<<<<<< HEAD
 # SPDX-License-Identifier: GPL-3.0-or-later
-__version__ = "0.1.5"
-=======
-# SPDX-License-Identifier: MIT
 __version__ = "0.1.6"
->>>>>>> 931f5d6 (⬆️ Bump version to 0.1.6)
-=======
-# SPDX-License-Identifier: MIT
-__version__ = "0.1.6"
-=======
-# SPDX-License-Identifier: GPL-3.0-or-later
-__version__ = "0.1.5"
->>>>>>> 0bcd920 (Update to GPLv3 license)
->>>>>>> d2121dd (Update to GPLv3 license)
